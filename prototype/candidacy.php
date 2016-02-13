ea99cbbc651d3128434f00812a74bf64ead3da0d
KADRI MOKS <kadrimoks@gmail.com>
2016-02-13 19:56:08 +0200

    added voting and candidacy

---
+++ prototype/candidacy.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  	<head>
+    	<?php require 'head.php'; ?>
+  	</head>
+  	<body>
+	  	<header class="container header">
+	  		<?php require 'header.php'; ?>
+		</header>
+		
+		
+		<section class="content container">
+	    	<div class="row">
+				<p>Käesolevaga nõustun, et ...</p>
+					<p>vali piirkond</p>
+						<select name="party">
+							<option>piirkond</option>
+							<option>piirkond</option>
+							<option>piirkond</option>
+						</select>
+	
+				<button type="candidacy">Kandideerin</button>
+			
+	    	</div>
+		</section>
+
+	    <?php require 'javascripts.php'; ?>
+  	</body>
+</html>
---
+++ prototype/vote.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  	<head>
+    	<?php require 'head.php'; ?>
+  	</head>
+  	<body>
+	  	<header class="container header">
+	  		<?php require 'header.php'; ?>
+		</header>
+		
+		
+		<section class="content container">
+	    	<div class="row">
+				<p>vali kandidaat</p>
+					<select name="party">
+								<option>kandidaat</option>
+								<option>kandidaat</option>
+								<option>kandidaat</option>
+							</select>
+	
+				<button type="candidacy">Valin</button>
+			
+	    	</div>
+		</section>
+
+	    <?php require 'javascripts.php'; ?>
+  	</body>
+</html>
