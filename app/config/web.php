2eca042bbbf434c42219a5fef3759a35225d4dc6
Sander <sander.sats@gmail.com>
2016-03-05 12:33:17 +0200

    remove app/web from siteurl

--- app/config/web.php
+++ app/config/web.php
@@ -2,6 +2,9 @@
 
 $params = require(__DIR__ . '/params.php');
 
+use \yii\web\Request;
+$baseUrl = str_replace('/app/web', '', (new Request)->getBaseUrl());
+
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
@@ -10,6 +13,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'XedPHVbpkwTqvTN41ntc8DAHq6BYIjfP',
+            'baseUrl' => $baseUrl,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
