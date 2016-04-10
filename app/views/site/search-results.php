2ab2985d8287b51b8358190c1bad44503a8f2f58
Sander <sander.sats@gmail.com>
2016-04-10 21:22:19 +0300

    Fixed search page labels

--- app/views/site/search-results.php
+++ app/views/site/search-results.php
@@ -6,28 +6,28 @@ $this->title = Yii::t('app', 'search');
 <h1><?= Yii::t('app', 'search') ?></h1>
 <form class="row">
 	<div class="col-md-12">
-		<label>
-			   <span title=<?= Yii::t('app', 'username help') ?>> <?= Yii::t('app', 'search') ?>: <input type="text" name="q">
-		</label>
-		<button type="submit"><?= Yii::t('app', 'search') ?></button>
+	   	<span title=<?= Yii::t('app', 'username help') ?>>
+			<label>
+			   		<?= Yii::t('app', 'search') ?>: <input type="text" name="q">
+			</label>
+			<button type="submit"><?= Yii::t('app', 'search') ?></button>
+	   	</span>
 	</div>
 	<div class="col-md-12">
-	
-	</span>
-		<label>
 		<span title=<?= Yii::t('app', 'candidate help') ?>> 
-			<?= Yii::t('app', 'candidate') ?>: <input type="checkbox" name="candidate" checked>
-		</label>
+			<label>
+					<?= Yii::t('app', 'candidate') ?>: <input type="checkbox" name="candidate" checked>
+			</label>
 		</span>
 		<span title=<?= Yii::t('app', 'region help') ?>> 
-		<label>
-			<?= Yii::t('app', 'region') ?>: <input type="checkbox" name="area">
-		</label>
+			<label>
+				<?= Yii::t('app', 'region') ?>: <input type="checkbox" name="area">
+			</label>
 		</span>
 		<span title=<?= Yii::t('app', 'party help') ?>> 
-		<label>
-			<?= Yii::t('app', 'party') ?>: <input type="checkbox" name="party">
-		</label>
+			<label>
+				<?= Yii::t('app', 'party') ?>: <input type="checkbox" name="party">
+			</label>
 		</span>
 	</div>
 </form>
